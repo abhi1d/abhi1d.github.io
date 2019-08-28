c13ff97e3bb38097521b1632ca7cd6f83f49dd58
Abhishek Sharma <Abhishek.Sharma@Yodlee.com>
2019-08-28 19:47:21 +0530

    Updates

---
+++ precache-manifest.99d98de2a54cdc5ec5ab28042da70aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae4fe5c438326946bfbcf8c074c3b5b",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "b1c045b6b26302f8fd4c",
+    "url": "/react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dff8727479fbfdcdfebe",
+    "url": "/react/static/js/2.1d0e8bc7.chunk.js"
+  },
+  {
+    "revision": "b1c045b6b26302f8fd4c",
+    "url": "/react/static/js/main.67a043b2.chunk.js"
+  },
+  {
+    "revision": "13f3e2330c5411c76016",
+    "url": "/react/static/js/runtime~main.2433e153.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
